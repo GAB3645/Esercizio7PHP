3efe6c5ad550dc9a606db6faf0afe5492a895882
Gabriel <gabrielsiano2006@gmail.com>
2024-09-24 13:36:49 +0400

    commit 1

---
+++ Esercizio7PHP.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+
+        $x = 10;
+        $y = 20;
+
+        if ($x > $y) {
+            echo "<h1> maggiore </h1>";
+        } else if ($x < $y) {
+            echo "<h1 >minore </h1>";
+        } else {
+            echo "<h1> uguale </h1>";
+        }
+
+        
+
+    ?>
+
+
+
+</body>
+</html>
